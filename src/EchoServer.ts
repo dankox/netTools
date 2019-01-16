74742396402ebc6c8bee7a5c616f4e178a544224
danko <konda02@ca.com>
2019-01-16 19:21:49 +0100

    interactive response implementation

--- src/EchoServer.ts
+++ src/EchoServer.ts
@@ -1,33 +1,36 @@
 import * as http from "http";
+import * as readlinesync from "readline-sync";
 
 export default class EchoServer {
 
     public static startServer(port: number) : void {
         console.log(`starting server on port: ${port}`);
         http.createServer(function(request,response){
+
             console.log(`${request.method} ${request.url}`);
-            // console.log(request.headers);
-            let tim: number;
-            let res = "GENERATE FAILED!";
-            if (String(request.url).indexOf("APIMSGS") > 0) {
-                res = "API: GENERATE FAILED!";
-                tim = 1000;
-            } else {
-                res = "C1: GENERATE FAILED!";
-                tim = 100;
-            }
-        
+            console.log(`\n## Request Header ##`);
+            console.log(JSON.stringify(request.headers, null, 2));
+            console.log(`## End of Header ##\n`);
+      
             request.on('data',function(message){
                 console.log(message.toString());
             });
             
             request.on('end',function(){
-                console.log("end of request! " + tim);
-                setTimeout(() => {
-                    response.write(res);
-                    response.end();
-                }, tim);
-                // response.end();
+                let myText : String = "";
+                let myResponse : String = "";
+                let first : boolean = true;
+                do {
+                    if (!first) {
+                        myResponse += `${myText}\n`;
+                    } else {
+                        first = false;
+                    }
+                    myText = readlinesync.prompt();
+                } while (myText != ":q" && myText != "quit")
+
+                response.write(myResponse);
+                response.end();
             }); 
         }).listen(port);
     }
